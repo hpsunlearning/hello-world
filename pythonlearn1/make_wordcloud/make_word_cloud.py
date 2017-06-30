fe3254a89b69e23bd0a8b3367db3640bbac4da3b
hpsunlearning <seagullsunhp@gmail.com>
2017-06-30 21:53:33 +0800

    wordcloud，set

--- pythonlearn1/make_wordcloud/make_word_cloud.py
+++ pythonlearn1/make_wordcloud/make_word_cloud.py
@@ -7,10 +7,8 @@ used for WordCloud generate
 
 from os import path
 from scipy.misc import imread
-from PIL import Image
 import matplotlib.pyplot as plt
 import jieba
-import numpy as np
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 def importStopword(filename=''):
@@ -31,23 +29,26 @@ def processChinese(text):
     return seg_list
 
 d = path.dirname('.')
-text = open(path.join(d, 'paper.txt'),encoding ='utf-8').read()
+stopwords = dict()
+importStopword(filename=path.join(d, 'stopwords.txt'))
+
+text = open(path.join(d, 'wukong.txt'),encoding ='utf-8').read()
 #text = processChinese(text)#对中文分词
 
-back_coloring = imread(path.join(d, "pic.jpg")) #一般配色
-#back_coloring = np.array(Image.open(path.join(d,"pic.jpg"))) #高阶配色
+back_coloring = imread(path.join(d, "wukong.jpg")) #一般配色
 
 wc = WordCloud(
 #                font_path =path.join(d, 'font.ttf'),#设置字体
                 background_color="white",#设置背景颜色
                 mask=back_coloring,#设置背景图片
+#                stopwords=STOPWORDS.add("said"),#设置停用词，默认STOPWORDS
                 max_words=2000,
                 max_font_size=40,
                 width=800,#设置图片大小
                 height=600,
 #                prefer_horizontal=0.9,#水平词出现频率0，-1
                 ranks_only=True,#用于频度的排序而不考虑实际频数的大小
-#                scale=1,#计算与绘制图像的比例，按比例放大
+                scale=1,#计算与绘制图像的比例，按比例放大
                 random_state=42
                 )
 
@@ -59,11 +60,6 @@ wc.generate(text)
 #wc.generate_from_frequencies(txt_freq)
 
 image_colors = ImageColorGenerator(back_coloring)
-
-#plt.imshow(wc)
-#plt.axis("off")
-#plt.figure()
-#######
 plt.imshow(wc.recolor(color_func=image_colors))
 plt.axis("off")
 plt.figure()
